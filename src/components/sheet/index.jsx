8623f5bb1f39b2859f2a8f7001e1612cbe74e46a
FVXOU <juanc.guerrerop@unac.edu.co>
2024-05-26 11:13:57 -0500

    Validación y estilado de las laminas especiales

--- src/components/sheet/index.jsx
+++ src/components/sheet/index.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./styles/index.css";
-import imgfondo from "../../assets/deck2.jpg";
+import sheetstarship from "../../assets/deckup4.webp";
+import sheetfilm from "../../assets/deck3.webp";
+import sheetcharacter from "../../assets/deck2.jpg";
 
-export const Sheet = ({ data, type }) => {
+export const Sheet = ({ data, info }) => {
+  const isSpecialSheet =
+    data.type === "films" ||
+    (data.type === "characters" && data.id >= 1 && data.id <= 20) ||
+    (data.type === "starships" && data.id >= 1 && data.id <= 10);
+  const getImageSheet = (type) => {
+    if (type === "films") {
+      return sheetfilm;
+    }
+    if (type === "characters") {
+      return sheetcharacter;
+    }
+    return sheetstarship;
+  };
   return (
-    <div className="sheet">
-      {type === "films" && <div>{data.title}</div>}
-      {type === "starships" ||
-        (type === "characters" && <div>{data.name}</div>)}
-      <img src={imgfondo} alt="" />
+    <div className={`sheet ${isSpecialSheet && "plus"}`}>
+      <div className="tag-id">{data.id}</div>
+      {info.title && <div className="sheet-tag">{info.title}</div>}
+      {info.name && <div className="sheet-tag">{info.name}</div>}
+      <img src={getImageSheet(data.type)} alt="" />
     </div>
   );
 };
