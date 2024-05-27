8e974cdf80290be9a1fbc74d06ebb98d5bf276b7
FVXOU <juanc.guerrerop@unac.edu.co>
2024-05-27 11:01:35 -0500

    Actualizacion de estilos del modal de visualización de laminas cuando al abrir sobre

--- src/components/modal-show-sheets/index.jsx
+++ src/components/modal-show-sheets/index.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import { Sheet } from "../sheet";
 import "./styles/index.css";
 
-export const ModalShowSheets = ({ sheets, onClose }) => {
+export const ModalShowSheets = ({ sheets, onClose, saveSheet }) => {
+  const handleClick = () => {
+    onClose();
+    saveSheet(sheets);
+  };
   return (
     <div className="modal-windown">
       <div className="modal-view">
-        {sheets !== undefined ? (
-          sheets.map((sheet, i) => <Sheet data={sheet} info={sheet.data} key={i}/>)
-        ) : (
-          <div>loading...</div>
-        )}
-      <button onClick={onClose}>Confirmar</button>
+        <div className="container-sheets">
+          {sheets !== undefined ? (
+            sheets.map((sheet, i) => (
+              <Sheet data={sheet} info={sheet.data} key={i} />
+            ))
+          ) : (
+            <div>loading...</div>
+          )}
+        </div>
+        <div className="container-button">
+          <button onClick={handleClick}>Confirmar</button>
+        </div>
       </div>
     </div>
   );
